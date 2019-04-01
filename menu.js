4542b19b8a1dff2d888431612b3c2ec18a254fcb
Ваше имяYevhen <johnyjust69@gmail.com>
2019-04-01 16:39:45 +0300

    y45

--- menu.js
+++ menu.js
@@ -1,52 +1,56 @@
 const button = document.querySelector('.menu__gamburger-link');
 const popup = document.querySelector('.menu__popup');
+const menuModalStyle = document.querySelector('.menu__modal');
 const menuModal = document.querySelector('.menu');
 const closedBtn = document.querySelector('.close');
 const closeStyle = document.querySelector('.menu__gamburger-link');
 const menuBtn = document.querySelectorAll('.menu__item-link');
 
+
 button.addEventListener('click', e => {
-    popup.classList.toggle('opened');
-    menuModal.classList.toggle('menu__modal');
-    closeStyle.classList.toggle('close');
-})
+    if (!(menuModal.classList.contains('menu__modal'))) {
+        menuModal.classList.add('menu__modal');
+        closeStyle.classList.add('close');
+        popup.classList.add('opened');
+        setTimeout(function foo() {
+            popup.style.opacity = '1';
+            menuModal.style.opacity = '1';
+        }, 30) 
+    }
+    else {
+        if (menuModal.style.opacity > 0) {
+            closeStyle.classList.remove('close');
+            setTimeout(function fooC() {
+                popup.style.opacity = '0';
+                menuModal.style.opacity = '0';
+            }, 30)
+            setTimeout(function fooM () {
+                menuModal.classList.remove('menu__modal');
+            }, 400)
+        }
+        else {
+            popup.classList.remove('opened');
+        }
+    }  
+});
 
 for (let i = 0; i < menuBtn.length; i++ ) {
     menuBtn[i].addEventListener('click', () => {
-        menuModal.classList.remove('menu__modal');
-        popup.classList.remove('opened');
-        closeStyle.classList.remove('close');
+        if (menuModal.style.opacity > 0) {
+            closeStyle.classList.remove('close');
+            setTimeout(function fooC() {
+                popup.style.opacity = '0';
+                menuModal.style.opacity = '0';
+            }, 30)
+            setTimeout(function fooM () {
+                menuModal.classList.remove('menu__modal');
+            }, 400)
+        }
+        else {
+            popup.classList.remove('opened');
+        }
     })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
