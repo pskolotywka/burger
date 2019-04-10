703cad32aae83d07ae0ac023d9ba5791f94d7897
Ваше имяYevhen <johnyjust69@gmail.com>
2019-04-10 13:54:19 +0300

    y67

--- team.js
+++ team.js
@@ -1,5 +1,6 @@
 
 const eBtn = document.querySelectorAll('.accordeon__item-name-button');
+const lishka = document.querySelectorAll('.accordeon__item');
 
 
 for (const item of eBtn) {
@@ -7,24 +8,32 @@ for (const item of eBtn) {
         const content = document.querySelectorAll('.accordeon__item-dropdown');
         const curItem = e.currentTarget;
         const btnParent = curItem.parentNode;
+        const li = btnParent.parentNode;
         const contentel = btnParent.nextElementSibling;
         const contBlock = contentel.firstElementChild;
-        const reqHeight = getComputedStyle(contBlock).height;
-        
-        if (curItem.classList.contains('active')) {
-            curItem.classList.remove('active');
+        const contBlockb = contBlock.firstElementChild;
+        const reqHeight = getComputedStyle(contBlockb).height;
+
+        if (li.classList.contains('active')) {
+            li.classList.remove('active');
             contentel.style.height = '0px';
+            setTimeout( function fooOut() {
+                contentel.style.animation = 'fadeOut .6s'
+            },10)
         }
         else {
             for (let i = 0; i < content.length; i++) {
                 content[i].style.height = '0px';
             }
-            for (let i = 0; i < eBtn.length; i++ ) {
-                eBtn[i].classList.remove('active');
+            for (let i = 0; i < lishka.length; i++ ) {
+                lishka[i].classList.remove('active');
             }
 
-            curItem.classList.add('active');
+            li.classList.add('active');
             contentel.style.height = reqHeight;
+            setTimeout( function fooIn() {
+                contentel.style.animation = 'fadeIn .6s'
+            },10)
         }
     })
 }
